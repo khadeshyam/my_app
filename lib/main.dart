cc0e49ed953643547c8a1bdfcddfb01498b9c6ce
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-01-23 17:20:15 +0530

    learn MaterialApp,Scaffold,AppBar,Center,Image,NetworkImage
    	learn : MaterialApp,Scaffold,AppBar,Center,Image,NetworkImage
    	widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+              title: Center(
+                child: Text("I am Shyam"),
+              ),
+              backgroundColor: Colors.blueGrey[900]),
+          body: Center(
+            child: Image(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=579&q=80')),
+          ),
+        ));
   }
 }
